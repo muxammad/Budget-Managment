de14d13c4a583d84a97c2cb893706c3abc13ae89
Muxammad <muxammadcodes@gmail.com>
2023-05-06 16:23:33 +0500

    entities

--- src/BudgetManagment.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BudgetManagment.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/BudgetManagment.Domain/Entities/Income.cs
@@ -0,0 +1,17 @@
+﻿using BudgetManagment.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManagment.Domain.Entities
+{
+    public class Income : Auditable
+    {
+        public int UserId { get; set; }
+        public User User { get; set; } = default!;
+        public decimal Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
